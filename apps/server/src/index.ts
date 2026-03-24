31bba924663e8608c45d330a805decc72976111b
Serenity Dev <dev@serenity.app>
2026-03-24 23:31:51 +0530

    Doing dumb things

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -22,7 +22,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:3002",
-      "https://serenitywellness.vercel.app/"
+      "https://serenitywellness.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
